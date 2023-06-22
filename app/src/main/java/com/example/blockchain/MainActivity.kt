97c59f29038b0469130b6f5e59f43e33aa619bc8
Agnesh <agnesh.skumar@gadgeon.com>
2023-06-22 18:36:10 +0530

    - Code cleanup - Added textview to display result

--- app/src/main/java/com/example/blockchain/MainActivity.kt
+++ app/src/main/java/com/example/blockchain/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.blockchain
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.blockchain.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.json.JSONObject
 import org.web3j.crypto.Credentials
 import org.web3j.protocol.Web3j
 import org.web3j.protocol.http.HttpService
@@ -22,37 +22,37 @@ import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding;
-    val contractAddress = "0x3caeb113c91d9475646e0c08ce81cf0a9cb3c612"
-    val privateKey =
+    private lateinit var binding: ActivityMainBinding
+    private val contractAddress = "0x3caeb113c91d9475646e0c08ce81cf0a9cb3c612"
+    private val privateKey =
         Credentials.create("47765422c2fdbd8f13377e20d90da1785b453d3d5aa682c851ebe825735164a9")
-    val url = "https://sepolia.infura.io/v3/81772628ac8649babbc528a8a4d3d376"
+    private val url = "https://sepolia.infura.io/v3/81772628ac8649babbc528a8a4d3d376"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.getATransaction.setOnClickListener(View.OnClickListener {
-            Toast.makeText(this, "Fetching details of a transaction...", Toast.LENGTH_SHORT).show();
+        binding.getATransaction.setOnClickListener {
+            Toast.makeText(this, "Fetching details of a transaction...", Toast.LENGTH_SHORT).show()
             val postData =
 //                "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getTransactionReceipt\",\"params\": [\"0x686806ed7b3166bb827321980471f2083ee5f9d7635a688f83b1120a6002597b\"],\"id\":1}"
                 "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getTransactionByHash\",\"params\": [\"0x2f334ec288ad6bb0a38eabe208efe7e363484d18dc3e9478b2ab4f393b338336\"],\"id\":1}"
-            postRequest(postData);
+            postRequest(postData, "transaction")
 
-        })
+        }
 
         binding.getTotalTransactionCount.setOnClickListener {
-            Toast.makeText(this, "Fetching total transaction count...", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Fetching total transaction count...", Toast.LENGTH_SHORT).show()
             val postData =
                 "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getTransactionCount\",\"params\": [\"0xE4AC1a31D59BA897091620E3F3d0e69520887B8c\",\"latest\"],\"id\":1}"
-            postRequest(postData);
+            postRequest(postData, "count")
         }
 
         binding.getWalletBalance.setOnClickListener {
             val postData =
                 "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBalance\",\"params\": [\"0xE4AC1a31D59BA897091620E3F3d0e69520887B8c\", \"latest\"],\"id\":1}"
-            postRequest(postData)
+            postRequest(postData, "count")
         }
 
         binding.performATransaction.setOnClickListener {
@@ -68,7 +68,7 @@ class MainActivity : AppCompatActivity() {
                     helloWorld.addRecord("test").send()
                     web3j.shutdown()
                 } catch (e: Exception) {
-                    Log.d("ERR", e.toString());
+                    Log.d("ERR", e.toString())
                 }
             }
         }
@@ -83,20 +83,20 @@ class MainActivity : AppCompatActivity() {
                         privateKey,
                         DefaultGasProvider()
                     )
-                    if (contract.isValid()) {
+                    if (contract.isValid) {
                         contract.addRecord("Road work at vytilla").send()
                         Log.d("TEST", contract.getnumberOfRecords().send().toString())
                     }
                     web3j.shutdown()
                 } catch (e: Exception) {
-                    Log.d("ERR", e.toString());
+                    Log.d("ERR", e.toString())
                 }
             }
         }
     }
 
 
-    private fun postRequest(postData: String) {
+    private fun postRequest(postData: String, action: String) {
         lifecycleScope.launch(Dispatchers.IO) {
             val url = URL("https://sepolia.infura.io/v3/81772628ac8649babbc528a8a4d3d376")
             val conn = url.openConnection() as HttpURLConnection
@@ -108,23 +108,36 @@ class MainActivity : AppCompatActivity() {
 
             DataOutputStream(conn.outputStream).use { it.writeBytes(postData) }
             BufferedReader(InputStreamReader(conn.inputStream)).use { br ->
-                var line: String?
-                while (br.readLine().also { line = it } != null) {
-                    //println(line)
-                    var separated = line!!.split(',')
-                    var data =
-                        separated[8].substring(separated[8].length - 65, separated[8].length - 1)
-                    data = removeTrailingZeroes(data)!!
-                    Log.d("DATA", data)
-                    hexToAscii(data)
+                var value: String?
+                var result = ""
+                while (br.readLine().also { value = it } != null) {
+                    println(value)
+                    val jsonObject = JSONObject(value)
+                    if (action.equals("count")) {
+                        var data = removeTrailingZeroes(jsonObject.getString("result"))
+                        data = data.subSequence(2, data.length).toString()
+                        result = hexToDecimal(data).toString()
+
+                    } else {
+                        val data = JSONObject(jsonObject.getString("result"))
+                        val fromAddress = data.getString("from")
+                        val toAddress = data.getString("to")
+                        val gas = data.getString("gas")
+                        val gasPrice = data.getString("gasPrice")
+                        result =
+                            "From: ${fromAddress}\nTo: ${toAddress}\ngas: ${gas}\ngasPrice: $gasPrice"
+                    }
+                }
+                runOnUiThread {
+                    binding.resultText.text = result
                 }
             }
         }
     }
 
-    fun removeTrailingZeroes(s: String?): String? {
-        val sb = java.lang.StringBuilder(s)
-        while (sb.length > 0 && sb[sb.length - 1] == '0') {
+    private fun removeTrailingZeroes(s: String?): String {
+        val sb = java.lang.StringBuilder(s.toString())
+        while (sb.isNotEmpty() && sb[sb.length - 1] == '0') {
             sb.setLength(sb.length - 1)
         }
         return sb.toString()
@@ -139,5 +152,9 @@ class MainActivity : AppCompatActivity() {
             i += 2
         }
         Log.d("DATA", output.toString())
+    }
+
+    private fun hexToDecimal(hex: String): Long {
+        return hex.toLong(16)
     }
 }
